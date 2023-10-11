abc5b8f5aaaa72faefc9c2d5b16e9d56753371ab
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-10-12 01:34:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+ const changeButton = document.getElementById('change_button');
+    const resetButton = document.getElementById('reset');
+
+    changeButton.addEventListener('click', () => {
+      const blockId = document.getElementById('block_id').value;
+      const color = document.getElementById('colour_id').value;
+      const gridItem = document.getElementById(blockId);
+
+      if (gridItem) {
+        // Reset background color of all grid items
+        const gridItems = document.getElementsByClassName('grid-item');
+        for (const item of gridItems) {
+          item.style.backgroundColor = 'transparent';
+        }
+
+        // Change the background color of the selected grid item
+        gridItem.style.backgroundColor = color;
+      }
+    });
+
+    resetButton.addEventListener('click', () => {
+      // Reset background color of all grid items
+      const gridItems = document.getElementsByClassName('grid-item');
+      for (const item of gridItems) {
+        item.style.backgroundColor = 'transparent';
+      }
+    });
